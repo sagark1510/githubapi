87001d7154fb3e0087e65a54ecaab0b3649e45db
Sagar Khatri <sagark1510@gmail.com>
2018-09-07 00:18:51 +0530

    Login with github done.

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import {Button} from 'react-native-elements';
+import OAuthManager from 'react-native-oauth';
 
 export default class Dashboard extends Component {
+  static navigationOptions = {
+    title: 'Dashboard',
+  };
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+  logout() {
+    const manager = new OAuthManager('githubapi');
+    manager.deauthorize('github');
+    this.props.navigation.replace('Login');
+  }
   render() {
     return (
-      <View>
-        <Text> Dashboard </Text>
+      <View style={styles.container}>
+        <Button title="Logout" onPress={this.logout} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
--- src/containers/login.js
+++ src/containers/login.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native';
+import {View, StyleSheet, Dimensions, Platform} from 'react-native';
 import {SocialIcon} from 'react-native-elements';
+import OAuthManager from 'react-native-oauth';
 
 const {width} = Dimensions.get('window');
+
+const configs = {
+  ios: {
+    client_id: '313cae832c28e42fd42e',
+    client_secret: '45cb3a685a26f3ae10fe178d98f01a52b4a7324b',
+  },
+  android: {
+    client_id: '8b74c236c1d3e2b5d6c4',
+    client_secret: 'af04b55b2704a6154960b5ccacffad867a118b76',
+  },
+};
 
 export default class Login extends Component {
   static navigationOptions = {
@@ -13,7 +25,17 @@ export default class Login extends Component {
     this.onLoginPress = this.onLoginPress.bind(this);
   }
   onLoginPress() {
-    alert('test');
+    const manager = new OAuthManager('githubapi');
+    manager.configure({
+      github: configs[Platform.OS],
+    });
+    manager
+      .authorize('github', {scopes: 'user:email read:user gist'})
+      .then(resp => {
+        console.log(resp);
+        this.props.navigation.replace('Dashboard');
+      })
+      .catch(err => console.log('There was an error', err));
   }
   render() {
     return (
